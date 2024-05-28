9680162e62118d9e4e772ce2f660ead8d78c4e58
hassan <nitexyber@gmail.com>
2024-05-28 22:55:51 +0500

    branch changes

--- app/src/main/java/com/example/samplecompose/io/mvpdemo2/presenters/LoginPresenter.kt
+++ app/src/main/java/com/example/samplecompose/io/mvpdemo2/presenters/LoginPresenter.kt
@@ -20,8 +20,8 @@ class LoginPresenter(var context: Context) : LoginContract.Presenter {
     override fun onLoginButtonClicked(email: String, password: String) {
 
         loginModel?.let {
-            Log.d(ConstValues.TAG, "P_MegaBranch" + email)
-            Log.d(ConstValues.TAG, "P_someChangsInMaster_Mistake" + password)
+            Log.d(ConstValues.TAG, "p :" + email)
+            Log.d(ConstValues.TAG, "p :" + password)
             val userResult = it.login(email, password)
 
            if (userResult)
